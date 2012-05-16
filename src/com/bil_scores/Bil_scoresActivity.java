d7c922c4698f4474a6ceaa1af6c97a18dfdbb900
Yakoff <y.lapushkin@mail.ru>
2012-05-16 23:49:39 +0400

    first commit

---
+++ src/com/bil_scores/Bil_scoresActivity.java
@@ -0,0 +1,13 @@
+package com.bil_scores;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Bil_scoresActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
